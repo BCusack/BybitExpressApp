4cc915c26e742255f2346ba1145d5a0a9124e9f5
BCusack <briancusack78@gmail.com>
2023-02-20 14:00:19 +1100

    modifies and simplified market req

--- routes/market.js
+++ routes/market.js
@@ -40,8 +40,40 @@ async function get_data(req) {
     const response = await axios.get(url);
     let result = response.data.result.filter(x => x.list.length > 0);
     result.forEach(element => {
-        candles_collection.push({ symbol: element.symbol, data: element.list });
+        let list = element.list.reverse();
+        let daily_candle = get_daily_candle(element.list);
+        let daily_pc_change = get_daily_pc_change(daily_candle, list[0]);
+        let total_volume = get_daily_volume_sum(list, daily_candle.startAt);
+        let all_daily_candles = get_daily_candles(list, daily_candle.startAt);
+        candles_collection.push({ symbol: element.symbol, abs_change: Math.abs(daily_pc_change), change: daily_pc_change, volume: total_volume });
+        candles_collection.sort((a, b) => b.abs_change - a.abs_change);
     });
 
     return candles_collection;
+}
+
+function get_daily_candle(candles) {
+    const current_time_utc = new Date(new Date().setHours(11, 0, 0, 0))
+    let current_day_utc = new Date(Math.floor(current_time_utc / 86400000) * 86400000);
+    let start_timestamp = Math.floor(current_day_utc.getTime() / 1000);
+    let candle_set = candles.filter(candle => candle.startAt === start_timestamp);
+    if (candle_set.length > 0) {
+        return candle_set[0];
+    }
+    return -1;
+}
+
+function get_daily_pc_change(daily_candle, candle_set) {
+    let change = (((candle_set['close'] - daily_candle['open']) / daily_candle['open']) * 100).toFixed(3);
+    return change;
+}
+
+function get_daily_volume_sum(candle_list, start_of_day) {
+    let candles = get_daily_candles(candle_list, start_of_day);
+    let total = candles.reduce((sum, candle) => sum + candle.volume, 0);
+    return parseInt(total);
+}
+
+function get_daily_candles(candle_list, start_of_day) {
+    return candle_list.filter(x => x.startAt >= start_of_day);
 }
